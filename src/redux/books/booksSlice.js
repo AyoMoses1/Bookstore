89db6a645b609aa6ac81f6e0554801fa5e2a5cf3
Ayo Moses <ayomoses111@gmail.com>
2023-03-09 23:49:35 +0100

    created middleware

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,27 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-  books: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  books: [],
+  status: 'idle',
+  error: '',
 };
+
+const BASE_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/LGxYf44oPI6RSiuKvX6C/books';
+
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  try {
+    const response = await axios.get(BASE_URL);
+    const res = response.data;
+    const books = Object.keys(res).map((key) => ({
+      item_id: key,
+      ...res[key][0],
+    }));
+    return books;
+  } catch (error) {
+    return error.message;
+  }
+});
 
 const bookSlice = createSlice({
   name: 'books',
@@ -35,6 +35,18 @@ const bookSlice = createSlice({
       newState.books = state.books.filter((book) => book.item_id !== action.payload);
       return newState;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchBooks.pending, (state, action) => {
+      state.status = 'Loading...';
+    }),
+    builder.addCase(fetchBooks.fulfilled, (state, action) => {
+      state.books = action.payload;
+      state.status = 'fulfilled';
+    });
+    builder.addCase(fetchBooks.rejected, (state, action) => {
+      state.error = action.payload;
+    });
   },
 });
 
